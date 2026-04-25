75a8c012a7138354768fdbe96153e612be4078a4
Alavya Bajpai <bajpaialavya@gmail.com>
2026-04-25 23:43:39 +0530

    Implement ATM functionality with balance management
    Added functions for checking balance, depositing, withdrawing, and viewing transaction history.

--- atm.py
+++ atm.py
@@ -1,6 +1,43 @@
+balance = 0
+transactions =[]
+
+
+def checkbalance():
+  print(f"Your Current Balance: {balance}")
+
+
+def deposit():
+  global balance
+  amount = int(input("Enter Amount :"))
+  balance += amount
+  transactions.append(f"Deposited {amount}")
+  print("Deposit Successful!")
+
+
+def withdraw():
+  global balance
+  amount = int(input("Enter Amount :"))
+  if amount <= balance:
+    balance -= amount
+    transactions.append(f"Withdrew {amount}")
+    print("Withdrawal successful!")
+  else:
+    print("Insufficient Balance")
+
+
+def statement():
+  print("\nTransactions History:")
+  if len(transacytions) == 0:
+    print("No Transactions Yet.")
+  else:
+    for i in transactions:
+      print("-", t)
+
+
 print("Welcome!")
 print("Please choose an option:")
 print("1.Check Amount")
 print("2.Deposite Amount")
 print("3.Withdraw")
-
+print("4.Statement")
+print("5.Exit")
